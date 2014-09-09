7c9c4f589b84fb96bd567ff9bfc98719390404db
Matt Glaman <nmd.matt@gmail.com>
2014-09-08 21:21:52 -0500

    Update IssueStatus to provide lookup from orgdinal, return Drupal.org style colors by issue status.

--- src/com/mglaman/drupalorg/IssueStatus.java
+++ src/com/mglaman/drupalorg/IssueStatus.java
@@ -1,5 +1,11 @@
 package com.mglaman.drupalorg;
 
+import android.graphics.Color;
+
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * PriorityLevel
  */
@@ -7,10 +13,23 @@ public enum IssueStatus {
     ACTIVE(1), FIXED(2), CLOSED_DUPLICATE(3), POSTPONED(4), CLOSED_WONTFIX(5), CLOSED_WORKSASDESIGNED(6),
     CLOSED_FIXED(7), NEEDS_REVIEW(8), NEEDS_WORK(13), RTBC(14), PATCH_TBP(15), POSTPONED_MORE_INFO(16),
     CLOSED_CANNOT_REPRODUCE(18);
+
     private final int levelID;
 
     private IssueStatus(int value) {
         this.levelID = value;
+    }
+
+    /**
+     * Returns IssueStatus from integer.
+     * @param n
+     * @return
+     */
+    public static IssueStatus fromOrdinal(int n) {
+        for(IssueStatus s : IssueStatus.values()) {
+            if(s.levelID == n) return s;
+        }
+        throw new IllegalArgumentException("Invalid issue status.");
     }
 
     /**
@@ -26,6 +45,33 @@ public enum IssueStatus {
             }
         }
         return null;
+    }
+
+    public int getColor() {
+        switch (this) {
+            case ACTIVE:
+                return Color.parseColor("#f9f9f9");
+            case FIXED:
+            case PATCH_TBP:
+            case RTBC:
+                return Color.parseColor("#d7ffd8");
+            case NEEDS_WORK:
+                return Color.parseColor("#ffece8");
+            case NEEDS_REVIEW:
+                return Color.parseColor("#ffffdd");
+            case POSTPONED:
+            case POSTPONED_MORE_INFO:
+                return Color.parseColor("#eff1f3");
+            case CLOSED_FIXED:
+            case CLOSED_WORKSASDESIGNED:
+            case CLOSED_WONTFIX:
+            case CLOSED_CANNOT_REPRODUCE:
+            case CLOSED_DUPLICATE:
+                return Color.parseColor("#fddddd");
+            default:
+                return Color.parseColor("#ffffff");
+
+        }
     }
 
     @Override
